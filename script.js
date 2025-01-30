6b4aa0b3aad8441edf4313774d8b90fdb2399618
Tayaba1234-frontend <usmantariq791664@gmail.com>
2025-01-30 11:13:20 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".search-bar-container");
+
+
+
+const magniFier = document.querySelector(".magnifire");
+
+
+
+magniFier.addEventListener("click", 
+    () => {
+        container.classList.toggle("active")
+    }
+)
